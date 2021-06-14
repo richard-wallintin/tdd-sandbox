5699732483c83c06ecbc07dc0fe8ab6c8664d0f5
Richard Wallintin <richard.wallintin@wps.de>
2021-06-14 15:46:23 +0200

    adds IT

--- src/main/kotlin/yummi/MenuController.kt
+++ src/main/kotlin/yummi/MenuController.kt
@@ -1,6 +1,11 @@
 package yummi
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class MenuController {
+    @GetMapping("menu")
     fun getMenu(): Menu {
         return Menu(
             Offer("Vanilla", Size.Small, Amount(2.5)),
