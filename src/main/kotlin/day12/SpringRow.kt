7b853c0c1db6ee0c2c49a3a841238a00c2944f60
Richard Wallintin <richard.wallintin@wps.de>
2023-12-17 00:07:17 +0100

    day 12 done (hard!)

---
+++ src/main/kotlin/day12/SpringRow.kt
@@ -0,0 +1,189 @@
+package day12
+
+import day12.SpringState.Companion.toState
+
+data class SpringRow(val states: List<SpringState>) {
+    fun groups(stateList: List<SpringState> = states): DamagedGroups {
+        return DamagedGroups(sequence {
+            var c = 0
+            stateList.forEach {
+                if (it == SpringState.DAMAGED) {
+                    c++
+                } else if (c > 0) {
+                    yield(c)
+                    c = 0
+                    if (it == SpringState.UNKNOWN)
+                        return@sequence
+                }
+            }
+            if (c > 0)
+                yield(c)
+        }.toList())
+    }
+
+    fun resolve() = resolveStates(states).map { SpringRow(it) }
+
+    private fun resolveStates(states: List<SpringState>): Sequence<List<SpringState>> {
+        val u = states.indexOfFirst { it == SpringState.UNKNOWN }
+        return if (u == -1) {
+            sequenceOf(states)
+        } else {
+            val head = states.subList(0, u)
+            val tail = states.drop(u + 1)
+
+            resolveStates(tail).flatMap {
+                sequenceOf(
+                    head + listOf(SpringState.DAMAGED) + it,
+                    head + listOf(SpringState.OPERATIONAL) + it
+                )
+            }
+        }
+    }
+
+    fun unfold(): SpringRow {
+        return SpringRow((1..5).map { states }.reduce { a, b ->
+            a + listOf(SpringState.UNKNOWN) + b
+        })
+    }
+
+    fun resolveTo(target: DamagedGroups) = resolveStatesTo(states, target).map { SpringRow(it) }
+
+    private fun resolveStatesTo(
+        states: List<SpringState>,
+        target: DamagedGroups
+    ): Sequence<List<SpringState>> {
+        val u = states.indexOfFirst { it == SpringState.UNKNOWN }
+        return if (u == -1) {
+            if (groups(states) == target) sequenceOf(states) else sequenceOf()
+        } else {
+            // TODO micro-optimize here: use subList AND dont re-build the list
+            val head = states.take(u)
+            val tail = states.drop(u + 1)
+
+            // TODO optimize here ---> drop more states earlier
+            sequenceOf(
+                head + listOf(SpringState.DAMAGED),
+                head + listOf(SpringState.OPERATIONAL)
+            )
+                .filter { groups(it) in target }
+                .map { it + tail }
+                .flatMap { resolveStatesTo(it, target) }
+        }
+    }
+
+    fun resolveCount(groups: DamagedGroups) = Resolver.resolveCount(
+        0,
+        states,
+        groups.groups
+    )
+
+    companion object {
+        fun parse(line: String) = SpringRow(line.asIterable().map { it.toState() })
+    }
+}
+
+object Resolver {
+    data class Step(
+        val currentGroup: Int,
+        val states: List<SpringState>,
+        val missingGroups: List<Int>
+    )
+
+    private val cache = mutableMapOf<Step, Long>()
+
+    fun resolveCount(
+        currentGroup: Int,
+        states: List<SpringState>,
+        missingGroups: List<Int>
+    ) = cache.getOrPut(
+        Step(currentGroup, states, missingGroups)
+    ) {
+        actualResolveCount(currentGroup, states, missingGroups)
+    }
+
+    private fun actualResolveCount(
+        currentGroup: Int,
+        states: List<SpringState>,
+        missingGroups: List<Int>
+    ): Long {
+        if (currentGroup > 0) {
+            val expectedGroup = missingGroups.firstOrNull() ?: return 0
+
+            if (currentGroup > expectedGroup)
+                return 0
+
+            if (states.isEmpty()) {
+                return resolveCount(0, states, missingGroups.dropLast(1))
+            }
+
+            val head = states.first()
+            val tail = states.subList(1, states.size)
+
+            return when (head) {
+                SpringState.UNKNOWN -> continueGroup(currentGroup, tail, missingGroups) +
+                        closeGroup(
+                            currentGroup,
+                            expectedGroup,
+                            tail,
+                            missingGroups
+                        )
+
+                SpringState.OPERATIONAL -> closeGroup(
+                    currentGroup,
+                    expectedGroup,
+                    tail,
+                    missingGroups
+                )
+
+                SpringState.DAMAGED -> continueGroup(currentGroup, tail, missingGroups)
+            }
+        }
+
+        if (states.isEmpty()) {
+            return if (missingGroups.isEmpty()) 1
+            else 0
+        }
+
+        if (states.count { it != SpringState.OPERATIONAL } < missingGroups.sum())
+            return 0
+
+        val head = states.first()
+        val tail = states.subList(1, states.size)
+
+        return when (head) {
+            SpringState.DAMAGED -> startGroup(tail, missingGroups)
+            SpringState.OPERATIONAL -> noGroup(tail, missingGroups)
+            SpringState.UNKNOWN -> startGroup(tail, missingGroups) + noGroup(tail, missingGroups)
+        }
+    }
+
+    private fun continueGroup(
+        currentGroup: Int,
+        tail: List<SpringState>,
+        missingGroups: List<Int>
+    ) = resolveCount(
+        currentGroup + 1,
+        tail,
+        missingGroups
+    )
+
+    private fun startGroup(
+        tail: List<SpringState>,
+        missingGroups: List<Int>
+    ) = resolveCount(1, tail, missingGroups)
+
+    private fun closeGroup(
+        currentGroup: Int,
+        nextGroup: Int,
+        tail: List<SpringState>,
+        missingGroups: List<Int>
+    ) = if (currentGroup != nextGroup) 0
+    else resolveCount(0, tail, missingGroups.drop(1))
+
+    private fun noGroup(
+        tail: List<SpringState>,
+        missingGroups: List<Int>
+    ) = resolveCount(
+        0, tail, missingGroups
+    )
+}
---
+++ src/test/kotlin/day12/SpringsTest.kt
@@ -0,0 +1,193 @@
+package day12
+
+import AOC
+import day12.ConditionRecord.Companion.totalResolutions
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class SpringsTest {
+
+    private val reference = """
+                ???.### 1,1,3
+                .??..??...?##. 1,1,3
+                ?#?#?#?#?#?#?#? 1,3,1,6
+                ????.#...#... 4,1,1
+                ????.######..#####. 1,6,5
+                ?###???????? 3,2,1
+            """.trimIndent()
+
+    private val input = AOC.getInput("/day12.txt")
+
+    private fun row(s: String) = SpringRow.parse(s)
+    private fun groups(vararg g: Int) = DamagedGroups(g.toList())
+
+    @Test
+    fun `parsing and representation`() {
+        SpringRow.parse("#.#.###") shouldBe SpringRow(
+            listOf(
+                SpringState.DAMAGED,
+                SpringState.OPERATIONAL,
+                SpringState.DAMAGED,
+                SpringState.OPERATIONAL,
+                SpringState.DAMAGED,
+                SpringState.DAMAGED,
+                SpringState.DAMAGED,
+            )
+        )
+
+        SpringRow.parse("???.###") shouldBe SpringRow(
+            listOf(
+                SpringState.UNKNOWN,
+                SpringState.UNKNOWN,
+                SpringState.UNKNOWN,
+                SpringState.OPERATIONAL,
+                SpringState.DAMAGED,
+                SpringState.DAMAGED,
+                SpringState.DAMAGED,
+            )
+        )
+    }
+
+    @Test
+    fun `parsing group stats`() {
+        DamagedGroups.parse("1,1,3") shouldBe DamagedGroups(listOf(1, 1, 3))
+    }
+
+    @Test
+    fun `complete records can compute groups`() {
+        SpringRow.parse("#").groups() shouldBe DamagedGroups(listOf(1))
+        SpringRow.parse("##").groups() shouldBe DamagedGroups(listOf(2))
+        SpringRow.parse("##.#").groups() shouldBe DamagedGroups(listOf(2, 1))
+        SpringRow.parse("#.#.###").groups() shouldBe DamagedGroups(listOf(1, 1, 3))
+    }
+
+    @Test
+    fun `resolve incomplete groups`() {
+        SpringRow.parse("#").resolve().toList() shouldBe listOf(SpringRow.parse("#"))
+        row("?").resolve().toList() shouldBe listOf(
+            SpringRow.parse("#"),
+            SpringRow.parse(".")
+        )
+        SpringRow.parse("??").resolve().toList() shouldBe listOf(
+            SpringRow.parse("##"),
+            SpringRow.parse(".#"),
+            SpringRow.parse("#."),
+            SpringRow.parse("..")
+        )
+    }
+
+    @Test
+    fun `count all matches`() {
+        ConditionRecord.parse("???.### 1,1,3").countResolutions() shouldBe 1
+    }
+
+    @Test
+    fun reference() {
+        reference.totalResolutions() shouldBe 21
+    }
+
+    @Test
+    fun `part 1`() {
+        input.totalResolutions() shouldBe 7460
+    }
+
+    @Test
+    fun unfold() {
+        ConditionRecord.parse(".# 1")
+            .unfold() shouldBe ConditionRecord.parse(".#?.#?.#?.#?.# 1,1,1,1,1")
+    }
+
+    @Test
+    fun `unfold reference resolution`() {
+        reference.totalResolutions(unfold = true) shouldBe 525152
+    }
+
+    @Test
+    fun `resolve towards`() {
+        row("?").resolveTo(DamagedGroups(listOf(1))).toList() shouldBe listOf(
+            SpringRow.parse("#")
+        )
+
+        SpringRow.parse("??").resolveTo(DamagedGroups(listOf(1))).toList() shouldBe listOf(
+            SpringRow.parse("#."),
+            SpringRow.parse(".#")
+        )
+    }
+
+    @Test
+    fun `partial groups only using prefix`() {
+        SpringRow.parse("#.#???").groups() shouldBe DamagedGroups(listOf(1, 1))
+        SpringRow.parse("#.#???#").groups() shouldBe DamagedGroups(listOf(1, 1))
+    }
+
+    @Test
+    fun `groups compatibility check head`() {
+        (DamagedGroups(listOf(1, 1)) in DamagedGroups(listOf(1, 2))) shouldBe true
+        (DamagedGroups(listOf(1, 2)) in DamagedGroups(listOf(1, 2))) shouldBe true
+        (DamagedGroups(listOf(1, 2)) in DamagedGroups(listOf(1, 2, 3, 5, 6))) shouldBe true
+        (DamagedGroups(listOf(1, 3)) in DamagedGroups(listOf(1, 2, 3, 5, 6))) shouldBe false
+        (DamagedGroups(listOf(1, 2, 3)) in DamagedGroups(listOf(1, 2, 3, 5, 6))) shouldBe true
+        (DamagedGroups(listOf(1, 1, 3)) in DamagedGroups(listOf(1, 2, 3, 5, 6))) shouldBe false
+    }
+
+    @Test
+    fun `part 2`() {
+        input.totalResolutions(unfold = true) shouldBe 6720660274964
+    }
+
+    @Test
+    fun `pure counting algorithm`() {
+        row(".").resolveCount(groups()) shouldBe 1
+
+        row(".").resolveCount(groups(1)) shouldBe 0
+        row("?").resolveCount(groups(2)) shouldBe 0
+        row("?#?").resolveCount(groups(4)) shouldBe 0
+
+        row("#").resolveCount(groups(1)) shouldBe 1
+        row("").resolveCount(groups()) shouldBe 1
+
+        row("?").resolveCount(groups(1)) shouldBe 1
+        row("??").resolveCount(groups(1)) shouldBe 2
+        row("???").resolveCount(groups(1)) shouldBe 3
+        row("???").resolveCount(groups(1)) shouldBe 3
+
+        row("???").resolveCount(groups(2)) shouldBe 2
+        row("??").resolveCount(groups(3)) shouldBe 0
+
+        row("???").resolveCount(groups(1, 2)) shouldBe 0
+
+        row("#??").resolveCount(groups(1, 2)) shouldBe 0
+        row("#??").resolveCount(groups(1)) shouldBe 1
+        row("#??").resolveCount(groups(2)) shouldBe 1
+        row("#???").resolveCount(groups(2, 1)) shouldBe 1
+        row("#???").resolveCount(groups(2, 2)) shouldBe 0
+
+        row("#.??").resolveCount(groups(2)) shouldBe 0
+        row("#.??").resolveCount(groups(1)) shouldBe 1
+        row("#.??").resolveCount(groups(1, 1)) shouldBe 2
+
+        row("..??").resolveCount(groups(2)) shouldBe 1
+        row("..??").resolveCount(groups(1)) shouldBe 2
+        row("??").resolveCount(groups(1)) shouldBe 2
+
+        row("??.").resolveCount(groups(1)) shouldBe 2
+        row("??.").resolveCount(groups(2)) shouldBe 1
+        row("??.").resolveCount(groups(3)) shouldBe 0
+        row("??#").resolveCount(groups(3)) shouldBe 1
+
+        row("?#?").resolveCount(groups(2)) shouldBe 2
+        row("?#??").resolveCount(groups(3)) shouldBe 2
+    }
+
+    @Test
+    fun `timing ref 1`() {
+        ConditionRecord.parse("#.????#?????????#??# 1,2,11,2").unfold()
+            .countResolutions() shouldBe 32
+    }
+
+    @Test
+    fun `timing ref 2`() {
+        ConditionRecord.parse("????.?????????? 1,1,2,2").unfold()
+            .countResolutions() shouldBe 6214236069570L
+    }
+}
