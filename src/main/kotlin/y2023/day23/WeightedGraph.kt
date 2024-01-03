5468f59d0698f2e8c90d4aafb206149d05340d8a
Richard Wallintin <richard.wallintin@wps.de>
2024-01-03 15:25:56 +0100

    generic weighted graph

---
+++ src/main/kotlin/y2023/day23/WeightedGraph.kt
@@ -0,0 +1,52 @@
+package y2023.day23
+
+import java.util.*
+
+data class Edge<N>(val nodes: Set<N>, val weight: Long) {
+    constructor(from: N, to: N, weight: Long) : this(setOf(from, to), weight)
+    constructor(from: N, to: N, weight: Int) : this(setOf(from, to), weight.toLong())
+
+    init {
+        assert(nodes.size == 2)
+    }
+
+    val from get() = nodes.first()
+    val to get() = nodes.last()
+
+    override fun toString(): String {
+        return "$from <=[$weight]=> $to"
+    }
+
+    fun other(node: N) = if (from == node) to else from
+    fun invert() = copy(weight = -weight)
+}
+
+data class WeightedGraph<N>(val edges: Set<Edge<N>>) {
+    private val links = mutableMapOf<N, Map<N, Long>>()
+
+    private fun links(n: N) = links.getOrPut(n) {
+        edges.filter { it.nodes.contains(n) }.map {
+            it.other(n) to it.weight
+        }.toMap()
+    }
+
+    inner class Path(val node: N, val length: Long = 0) {
+        fun next() =
+            links(node).entries.map { (n, w) -> Path(node = n, length = length + w) }
+    }
+
+    fun shortestPath(start: N, finish: N, heuristic: (N, N) -> Long) = sequence {
+        val q = PriorityQueue(compareBy<Path> { it.length + heuristic(it.node, finish) })
+        q.add(Path(start))
+
+        while (q.isNotEmpty()) {
+            val p = q.remove()
+
+            if (p.node == finish) yield(p.length)
+
+            q.addAll(p.next())
+        }
+    }
+
+    fun invert() = copy(edges = edges.map { it.invert() }.toSet())
+}
--- src/test/kotlin/y2023/day23/HikeTest.kt
+++ src/test/kotlin/y2023/day23/HikeTest.kt
@@ -3,6 +3,7 @@ package y2023.day23
 import AOC
 import io.kotest.matchers.ints.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import util.CardinalDirection
 import util.Point
@@ -66,7 +67,63 @@ class HikeTest {
     }
 
     @Test
+    @Disabled("still tooo slooooow")
     fun `part 2`() {
         islandMap.longestHikeLengthIgnoringSlopes.also(::println) shouldBeGreaterThan 2106
+    }
+
+    @Test
+    fun `convert map to weighted graph`() {
+        IslandMap.of(
+            """
+            #.###
+            #..##
+            ##..#
+            ###.#
+            ###.#
+        """.trimIndent()
+        ).toGraph() shouldBe WeightedGraph(
+            setOf(
+                Edge(Point(1, 0), Point(3, 4), 6)
+            )
+        )
+    }
+
+    @Test
+    fun `convert to complex graph`() {
+        val graph = IslandMap.of(
+            """
+            #.###
+            .....
+            .###.
+            .....
+            ##.##
+        """.trimIndent()
+        ).toGraph()
+
+        graph shouldBe WeightedGraph(
+            setOf(
+                Edge(Point(1, 0), Point(1, 1), 1),
+                Edge(Point(1, 1), Point(2, 3), 5),
+                Edge(Point(1, 1), Point(2, 3), 7),
+                Edge(Point(2, 3), Point(2, 4), 1),
+            )
+        )
+
+        graph.shortestPath(Point(1, 0), Point(2, 4), Point::distance)
+            .first() shouldBe 7
+    }
+
+    @Test
+    fun `invert a weighted graph`() {
+        WeightedGraph(
+            setOf(
+                Edge(Point(1, 0), Point(3, 4), 6)
+            )
+        ).invert() shouldBe WeightedGraph(
+            setOf(
+                Edge(Point(1, 0), Point(3, 4), -6)
+            )
+        )
     }
 }
