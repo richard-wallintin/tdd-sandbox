ca81f12114948db11fcc06ab447867e592384697
Richard Wallintin <richard.wallintin@wps.de>
2024-12-02 16:37:22 +0100

    2024/day1

---
+++ src/main/kotlin/y2024/LocationList.kt
@@ -0,0 +1,30 @@
+package y2024
+
+import util.integers
+import kotlin.math.abs
+
+data class LocationList(val list: List<Pair<Int, Int>>) {
+    fun totalDistance(): Int {
+        return list.sumOf { (a, b) -> abs(a - b) }
+    }
+
+    private val left = list.map { it.first }
+    private val right = list.map { it.second }
+
+    fun sort(): LocationList {
+        return LocationList(left.sorted().zip(right.sorted()))
+    }
+
+    fun similarityScore(): Int {
+        return left.sumOf { l -> l * right.count { it == l } }
+    }
+
+    companion object {
+        fun parse(str: String): LocationList {
+            return str.lineSequence().filter { it.isNotBlank() }.map {
+                val (a, b) = it.integers()
+                Pair(a, b)
+            }.toList().let(::LocationList)
+        }
+    }
+}
---
+++ src/test/kotlin/y2024/LocationListTest.kt
@@ -0,0 +1,63 @@
+package y2024
+
+import AOC
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class LocationListTest {
+    private val referenceList = LocationList(
+        listOf(
+            Pair(3, 4),
+            Pair(4, 3),
+            Pair(2, 5),
+            Pair(1, 3),
+            Pair(3, 9),
+            Pair(3, 3)
+        )
+    )
+
+    @Test
+    fun `two lists`() {
+        LocationList.parse(
+            """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        """.trimIndent()
+        ) shouldBe referenceList
+    }
+
+    @Test
+    fun totalDistance() {
+        LocationList(
+            listOf(
+                Pair(3, 4),
+                Pair(4, 3),
+                Pair(2, 5),
+                Pair(1, 3),
+                Pair(3, 9),
+                Pair(3, 3)
+            )
+        ).sort().totalDistance() shouldBe 11
+    }
+
+    private val inputList = LocationList.parse(AOC.getInput("/2024/day1.txt"))
+
+    @Test
+    fun part1() {
+        inputList.sort().totalDistance() shouldBe 3508942
+    }
+
+    @Test
+    fun `similarity scoring`() {
+        referenceList.similarityScore() shouldBe 31
+    }
+
+    @Test
+    fun part2() {
+        inputList.similarityScore() shouldBe 26593248
+    }
+}
