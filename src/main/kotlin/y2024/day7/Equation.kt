24b0ffcd494d2bed59630508c7c771358f741a86
Richard Wallintin <richard.wallintin@wps.de>
2024-12-07 19:02:41 +0100

    2024/day7/pt2

--- src/main/kotlin/y2024/day7/Equation.kt
+++ src/main/kotlin/y2024/day7/Equation.kt
@@ -6,15 +6,17 @@ data class Equation(
     val operands: List<Long>,
     val result: Long
 ) {
-    fun solutions() = results(operands.reversed()).count { it == result }
+    fun solutions(concat: Boolean = false) =
+        results(operands.reversed(), concat).count { it == result }
 
-    private fun results(tail: List<Long>): Sequence<Long> = sequence {
-        val l = tail.firstOrNull() ?: return@sequence
-        val r = tail.drop(1)
-        if (r.isEmpty()) yield(l)
-        else results(r).forEach {
-            yield(l + it)
-            yield(l * it)
+    private fun results(head: List<Long>, concat: Boolean = false): Sequence<Long> = sequence {
+        val right = head.firstOrNull() ?: return@sequence
+        val left = head.drop(1)
+        if (left.isEmpty()) yield(right)
+        else results(left, concat).forEach { leftResult ->
+            yield(right + leftResult)
+            yield(right * leftResult)
+            if (concat) yield((leftResult.toString() + right.toString()).toLong())
         }
     }
 
@@ -26,7 +28,7 @@ data class Equation(
 
         fun parseMany(text: String) = text.lineSequence().map { parse(it) }
 
-        fun Sequence<Equation>.totalCalibrationResult() =
-            filter { it.solutions() > 0 }.sumOf { it.result }
+        fun Sequence<Equation>.totalCalibrationResult(concat: Boolean = false) =
+            filter { it.solutions(concat) > 0 }.sumOf { it.result }
     }
 }
--- src/test/kotlin/y2024/day7/CalibrationTest.kt
+++ src/test/kotlin/y2024/day7/CalibrationTest.kt
@@ -35,27 +35,46 @@ class CalibrationTest {
         Equation.parse("83: 17 5").solutions() shouldBe 0
     }
 
+    private val sampleEquations = Equation.parseMany(
+        """
+                190: 10 19
+                3267: 81 40 27
+                83: 17 5
+                156: 15 6
+                7290: 6 8 6 15
+                161011: 16 10 13
+                192: 17 8 14
+                21037: 9 7 18 13
+                292: 11 6 16 20
+            """.trimIndent()
+    )
+
     @Test
     fun `total calibration result`() {
-        Equation.parseMany(
-            """
-            190: 10 19
-            3267: 81 40 27
-            83: 17 5
-            156: 15 6
-            7290: 6 8 6 15
-            161011: 16 10 13
-            192: 17 8 14
-            21037: 9 7 18 13
-            292: 11 6 16 20
-        """.trimIndent()
-        ).totalCalibrationResult() shouldBe 3749
+        sampleEquations.totalCalibrationResult() shouldBe 3749
     }
+
+    private val inputEquations = Equation.parseMany(AOC.getInput("/2024/day7.txt"))
 
     @Test
     fun part1() {
-        Equation.parseMany(AOC.getInput("/2024/day7.txt"))
+        inputEquations
             .totalCalibrationResult() shouldBe 932137732557L
+    }
+
+    @Test
+    fun `solve by concat`() {
+        Equation.parse("156: 15 6").solutions(true) shouldBe 1
+    }
+
+    @Test
+    fun `add concat operator`() {
+        sampleEquations.totalCalibrationResult(concat = true) shouldBe 11387
+    }
+
+    @Test
+    fun part2() {
+        inputEquations.totalCalibrationResult(concat = true) shouldBe 661823605105500L
     }
 }
 
