d3ca7b55297197196580f93a944c3188606d373c
Richard Wallintin <richard.wallintin@wps.de>
2025-01-12 00:31:51 +0100

    2024/day22/pt2

--- src/test/kotlin/y2024/day22/BuyerTest.kt
+++ src/test/kotlin/y2024/day22/BuyerTest.kt
@@ -1,9 +1,12 @@
 package y2024.day22
 
 import AOC
+import io.kotest.matchers.maps.shouldContain
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import util.forever
+import y2024.day22.SecretNumber.Companion.buildIndex
+import y2024.day22.SecretNumber.Companion.mostBananas
 import y2024.day22.SecretNumber.Companion.sum
 
 class BuyerTest {
@@ -47,18 +50,52 @@ class BuyerTest {
         )
     }
 
+    private val inputMarket = SecretNumber.parseMany(AOC.getInput("/2024/day22.txt"))
+
     @Test
     fun part1() {
-        SecretNumber.parseMany(AOC.getInput("/2024/day22.txt")).map {
-            it.next(2000)
-        }.sum() shouldBe 15006633487UL
+        inputMarket.map { it.next(2000) }.sum() shouldBe 15006633487UL
     }
 
 
+    @Test
+    fun `compute prices`() {
+        SecretNumber.of(123).start().take(10).map { it.price }.toList() shouldBe listOf(
+            3, 0, 6, 5, 4, 4, 6, 4, 4, 2
+        )
+    }
+
+    @Test
+    fun `sell at sequence`() {
+        SecretNumber.of(123).sellAt(listOf(-1, -1, 0, 2)) shouldBe 6
+    }
+
+    @Test
+    fun `multiple sales`() {
+        listOf(1, 2, 3, 2024).map { SecretNumber.of(it) }.sumOf {
+            it.sellAt(listOf(-2, 1, -1, 3))
+        } shouldBe 23
+    }
+
+    @Test
+    fun `build buyer index`() {
+        val buyerIndex = listOf(1, 2, 3, 2024).map { SecretNumber.of(it) }.buildIndex()
+        buyerIndex shouldContain (listOf(-2, 1, -1, 3) to 23)
+        buyerIndex.mostBananas() shouldBe 23
+    }
+
+    @Test
+    fun part2() {
+        inputMarket.toList().buildIndex().mostBananas() shouldBe 1710
+    }
 }
+
+typealias Seq = List<Int>
 
 @JvmInline
 value class SecretNumber(val value: UInt) {
+    val price: Int get() = (value % 10u).toInt()
+
     fun next(): SecretNumber {
         var s = value
         s = prune((s shl 6) xor s)
@@ -73,6 +110,17 @@ value class SecretNumber(val value: UInt) {
     override fun toString() = value.toString(10)
     fun next(n: Int) = start().elementAt(n)
 
+    fun sellAt(seq: List<Int>) =
+        delta4().firstOrNull { it.matches(seq) }?.price ?: 0
+
+
+    private fun delta4() = start().take(2000).map { it.price }
+        .zipWithNext { old, price -> price to (price - old) }
+        .zipWithNext { prev, cur -> PriceSpot(cur.first, listOf(prev.second, cur.second)) }
+        .zipWithNext(PriceSpot::extend)
+        .zipWithNext(PriceSpot::extend)
+
+
     companion object {
         private const val B24: UInt = 0xFFFFFFu
 
@@ -82,6 +130,34 @@ value class SecretNumber(val value: UInt) {
 
         fun parseMany(text: String) = text.lineSequence().map { SecretNumber(it.toUInt()) }
         fun Sequence<SecretNumber>.sum() = sumOf { it.value.toULong() }
+
+        fun List<SecretNumber>.buildIndex(): Map<Seq, Int> {
+            data class Entry(var lastBuyer: Int = -1, var bananas: Int = 0)
+
+            val idx = mutableMapOf<Seq, Entry>()
+
+            this.forEachIndexed { buyerNo, secret ->
+                secret.delta4().forEach { spot ->
+                    val e = idx.getOrPut(spot.seq, ::Entry)
+                    if (e.lastBuyer < buyerNo) {
+                        e.lastBuyer = buyerNo
+                        e.bananas += spot.price
+                    }
+                }
+            }
+
+            return idx.mapValues { (_, v) -> v.bananas }
+        }
+
+        fun Map<Seq, Int>.mostBananas() = values.max()
     }
 
+    data class PriceSpot(val price: Int, val seq: Seq) {
+        private val length get() = seq.size
+
+        private fun combine(prev: PriceSpot) = copy(seq = prev.seq + seq.drop(length - 1))
+        fun extend(next: PriceSpot) = next.combine(this)
+
+        fun matches(seq: Seq) = this.seq == seq
+    }
 }
